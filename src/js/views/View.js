b44ec7fef8d3f66f11527780d087727a487432d8
Heemo Yang <heemo521@gmail.com>
2022-03-22 15:43:28 -0700

    Update serving size

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -6,9 +6,7 @@ export default class View {
   render(data) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
-
     this._data = data;
-
     const markup = this._generateMarkup();
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
