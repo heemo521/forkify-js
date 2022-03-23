2fcc255e8c2a3f969d9b0ed2419505246fa367b9
Heemo Yang <heemo521@gmail.com>
2022-03-23 00:08:34 -0700

    Finish project

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,7 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
+
+export const KEY = `0807adfb-51ff-4972-9420-b2b08f4f7b6b`;
+
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,28 +1,33 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class PreviewView extends View {
   _parentElement = '';
 
-  //id, image, publisher, title
   _generateMarkup() {
-    // preview__link--active
     const id = window.location.hash.slice(1);
 
-    return `        
-    <li class="preview">
+    return `
+      <li class="preview">
         <a class="preview__link ${
           this._data.id === id ? 'preview__link--active' : ''
         }" href="#${this._data.id}">
-            <figure class="preview__fig">
-                <img src="${this._data.image}" alt="${this._data.title}" />
-            </figure>
-            <div class="preview__result">
-                <h4 class="preview__title">${this._data.title}</h4>
-                <p class="preview__publisher">${this._data.publisher}</p>
+          <figure class="preview__fig">
+            <img src="${this._data.image}" alt="${this._data.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${this._data.title}</h4>
+            <p class="preview__publisher">${this._data.publisher}</p>
+            <div class="preview__user-generated ${
+              this._data.key ? '' : 'hidden'
+            }">
+              <svg>
+              <use href="${icons}#icon-user"></use>
+              </svg>
             </div>
+          </div>
         </a>
-    </li>
+      </li>
     `;
   }
 }
